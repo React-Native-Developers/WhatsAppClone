bc15a2419eb8f37bfb7d2ceeed0f6b1cbee65701
Diego Alysson Braga Moreira <diegoalyssonbm@gmail.com>
2018-04-21 01:42:14 -0300

    Add Initial Redux files.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Routes from './src/Routes'
+import reducers from './src/reducers'
 
-export default class App extends Component {
-  render() {
-    return (
+export default props => (
+  <Provider store={createStore(reducers)}>
       <Routes />
-    );
-  }
-}
+  </Provider>
+);
